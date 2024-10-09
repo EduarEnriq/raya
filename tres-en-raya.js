cb209857382d46cc7529a6a6b90cb9b91f7b6343
Eduardo <130516165+EduarEnriq@users.noreply.github.com>
2024-10-08 21:03:10 -0400

    Add files via upload

---
+++ tres-en-raya.js
@@ -0,0 +1,35 @@
+let turn = 0;
+const tablero = [];
+
+const btnPulsado = (e,pos) =>{
+    turn ++;
+    const btn = e.target;
+    const color = turn % 2 ? 'red': 'blue'
+    btn.style.backgroundColor = color;
+    tablero[pos] = color;
+    if(haGanado()){alert('Ganaste')}
+}
+
+const haGanado = () =>{
+    if(tablero[0] == tablero[1] && tablero[0] == tablero[2] && tablero[0]){
+        return true;
+    }else if(tablero[3] == tablero[4] && tablero[3] == tablero[5] && tablero[3]){
+        return true;
+    }else if(tablero[6] == tablero[7] && tablero[6] == tablero[8] && tablero[6]){
+        return true;
+    }else if(tablero[0] == tablero[3] && tablero[0] == tablero[6] && tablero[0]){
+        return true;
+    }else if(tablero[1] == tablero[4] && tablero[1] == tablero[7] && tablero[1]){
+        return true;
+    }else if(tablero[2] == tablero[5] && tablero[2] == tablero[8] && tablero[2]){
+        return true;
+    }else if(tablero[0] == tablero[4] && tablero[0] == tablero[8] && tablero[0]){
+        return true;
+    }else if(tablero[2] == tablero[4] && tablero[2] == tablero[6] && tablero[2]){
+        return true;
+    }
+    return false;
+}
+
+document.querySelectorAll('button').forEach(
+    (obj,i) => obj.addEventListener('click',(e)=> btnPulsado(e,i)));
